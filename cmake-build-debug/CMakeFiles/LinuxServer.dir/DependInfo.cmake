08dfad22f3b977f4beab1d40efc10704beaa34b6
benson <bensonuouououo@gmail.com>
2020-04-17 00:45:21 -0700

    “init”

---
+++ cmake-build-debug/CMakeFiles/LinuxServer.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/huangbenson/Desktop/correct/CGImysql/sql_connection_pool.cpp" "/home/huangbenson/Desktop/correct/cmake-build-debug/CMakeFiles/LinuxServer.dir/CGImysql/sql_connection_pool.cpp.o"
+  "/home/huangbenson/Desktop/correct/http/http_conn.cpp" "/home/huangbenson/Desktop/correct/cmake-build-debug/CMakeFiles/LinuxServer.dir/http/http_conn.cpp.o"
+  "/home/huangbenson/Desktop/correct/log/log.cpp" "/home/huangbenson/Desktop/correct/cmake-build-debug/CMakeFiles/LinuxServer.dir/log/log.cpp.o"
+  "/home/huangbenson/Desktop/correct/main.cpp" "/home/huangbenson/Desktop/correct/cmake-build-debug/CMakeFiles/LinuxServer.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../threadpool"
+  "../log"
+  "../CGImysql"
+  "../http"
+  "../lock"
+  "../timer"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/LinuxServer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LinuxServer.dir/CGImysql/sql_connection_pool.cpp.o"
+  "CMakeFiles/LinuxServer.dir/http/http_conn.cpp.o"
+  "CMakeFiles/LinuxServer.dir/log/log.cpp.o"
+  "CMakeFiles/LinuxServer.dir/main.cpp.o"
+  "LinuxServer"
+  "LinuxServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LinuxServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
